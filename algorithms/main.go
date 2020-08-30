d842068934161fed5c64953bd2b5f4a97d7463b6
Pai <slashpai9@gmail.com>
2020-08-30 20:32:22 +0530

    go fmt

--- algorithms/main.go
+++ algorithms/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/slashpai/computer_science/algorithms/searching"
 	"fmt"
+	"github.com/slashpai/computer_science/algorithms/searching"
 )
 
 func main() {
-	list :=[]int {2, 3, 4, 5, 8, 9, 20, 25, 39 }
-	result := searching.Search(list, 0, len(list) - 1, 1)
+	list := []int{2, 3, 4, 5, 8, 9, 20, 25, 39}
+	result := searching.Search(list, 0, len(list)-1, 1)
 	fmt.Println("Element found? :", result)
 }
